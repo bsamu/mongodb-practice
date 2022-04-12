278504fb2c8534c561a930fe05b78f70578abafb
bsamu <bsamuel0220@gmail.com>
2022-04-12 11:30:08 +0200

    insomnia post workin

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
+// const mongoose = require('mongoose');
+// const express = require('express')
+// const cors = require('cors')
+
+
+// const app = express()
+// const port = 3000
+// mongoose.connect('mongodb://localhost:27017/test');
+
+// app.use(cors());
+// app.use(express.json());
+
+// const Dogs = mongoose.model('Dogs', { name: String });
+
+// app.post('/api/dog', (req, res) => {
+//     const puppy = new Dogs({ name: req.body.name });
+//     puppy.save().then(() => res.send('created'));
+// })
+
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+// })
+
 const mongoose = require('mongoose');
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const port = 3000
 mongoose.connect('mongodb://localhost:27017/test');
 
-// const Cat = mongoose.model('Cat', { name: String });
-const Dog = mongoose.model('Dog', { name: String });
+app.use(cors());
+app.use(express.json());
 
-// const kitty = new Cat({ name: 'Zildjian' });
-const puppy = new Dog({ name: 'Zilgamesh' })
-// kitty.save().then(() => console.log('meow'));
-puppy.save().then(() => console.log('vau'));
+const Cat = mongoose.model('Cat', { name: String })
+
+app.post('/api/dog', (req, res) => {
+    const puppy = new Cat({ name: req.body.name });
+    puppy.save().then(() => res.send('created'));
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
